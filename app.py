48f80b8dff0a11bf3c97841d3ef21e962cdbaff2
reasure3 <reasure3@naver.com>
2026-03-13 11:42:37 +0900

    feat: add home endpoint

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return jsonify({
         "message": "Welcome to the Flask Study Notes Service",
         "description": "This project is for practicing Git workflow with Flask.",
+        "available_endpoints": [
+            "/",
+            "/home"
+        ]
     })
 
 
